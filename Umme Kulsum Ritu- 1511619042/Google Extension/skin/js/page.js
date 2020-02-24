77b336be41d0eb041a209a464ae1a788a1406877
Umme Kulsum Ritu <umme.ritu@northsouth.edu>
2020-02-25 00:17:53 +0600

    javascript files

---
+++ Umme Kulsum Ritu- 1511619042/Google Extension/skin/js/page.js
@@ -0,0 +1,19 @@
+
+function simple_screenshot_overlay_toggle() {
+
+	if (jQuery('body').find('.simple_screenshot_bg').length == 0) {
+		jQuery('body').append('<div class="simple_screenshot_bg"></div>');
+		jQuery('.simple_screenshot_bg').Jcrop({
+			onSelect: function (c) {
+				chrome.extension.sendMessage({
+					screenshotCoords: c,
+				});
+				jQuery('.simple_screenshot_bg').remove();
+			},
+		});
+	} else {
+		jQuery('.simple_screenshot_bg').remove();
+
+	}
+}
+
